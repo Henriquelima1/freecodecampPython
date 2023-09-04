0a08f27d43f5fc02c8cd05bb35ebf26f6ec261c0
Henriquelima1 <45356318+Henriquelima1@users.noreply.github.com>
2023-09-04 16:25:13 -0300

    Update Formatador Aritmético.py

--- Formatador Aritmético.py
+++ Formatador Aritmético.py
@@ -1,16 +1,45 @@
 import re
-def arithmetic_arranger(vetor):
-  mat =[]
-  for i in vetor:
-    componentes = re.split(r'\s*([\+\-\*/])\s*', i)
-    comprimento = len(componentes[0])
-    if(comprimento <= len(componentes[2]))
-      comprimento = len(componentes[2]
 
-    
-    mat[0][i] = componentes[0]
-    mat[1][i] = componentes[1] + " " + componentes[2]
-    mat[2][i] = "-----"
+def arithmetic_arranger(problems, display_results=False):
+    if len(problems) > 5:
+        return "Error: Too many problems."
 
-  
+    mat = [[], [], []]  # Apenas três elementos para os problemas originais
+    results = []  # Lista para armazenar os resultados
+
+    for problem in problems:
+        # Usar uma expressão regular para separar os componentes do problema
+        componentes = re.split(r'\s*([\+\-])\s*', problem)
+        if len(componentes) != 3:
+            return "Error: Operator must be '+' or '-'."
+
+        num1, operador, num2 = componentes
+        if operador not in "+-":
+            return "Error: Operator must be '+' or '-'."
+        if not (num1.isdigit() and num2.isdigit()):
+            return "Error: Numbers must only contain digits."
+
+        if len(num1) > 4 or len(num2) > 4:
+            return "Error: Numbers cannot be more than four digits."
+
+        comprimento = max(len(num1), len(num2)) + 2
+
+        mat[0].append(num1.rjust(comprimento))
+        mat[1].append(operador + ' ' + num2.rjust(comprimento - 2))
+        mat[2].append('-' * comprimento)
+
+        if operador == "+":
+            num3 = str(int(num1) + int(num2))
+        elif operador == "-":
+            num3 = str(int(num1) - int(num2))
+        else:
+            return "Error: Unsupported operator."
+
+        results.append(num3.rjust(comprimento))  # Adicionar o resultado à lista de resultados
+
+    arranged_problems = '\n'.join(['    '.join(mat[i]) for i in range(3)])
+
+    if display_results:
+        arranged_problems += '\n' + '    '.join(results)  # Adicionar os resultados ao final
+
     return arranged_problems
